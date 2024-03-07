add67c5f96d5099037226ee0f18c88b69481b113
Youness <younesssynester9@gmail.com>
2024-03-07 20:01:17 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int randomNumber = (int) (Math.random() * (100 - 1));
+
+
+        if(randomNumber%2 == 0)
+        {
+            System.out.println(randomNumber + " is an even number");
+        }
+        else {
+            System.out.println(randomNumber + " is an odd number");
+        }
+    }
+}
